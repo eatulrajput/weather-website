e18726e61f979b885779d051f3414c674c9cf409
Atul Rajput <eatulrajput@gmail.com>
2024-06-10 22:39:17 +0530

    Updating Repository

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,11 @@
+document.getElementById('menu-toggle').addEventListener('click', () => {
+  document.getElementById('nav-links').classList.toggle('show');
+});
+
+document.getElementById('menu-close').addEventListener('click', () => {
+  document.getElementById('nav-links').classList.remove('show');
+});
+
 const options = {
   method: 'GET',
   headers: {
@@ -6,35 +14,35 @@ const options = {
   }
 };
 
-const getWeather =(city) =>{
-  cityName.innerHTML = city
+const getWeather = (city) => {
+  document.getElementById('cityName').innerHTML = city;
 
-fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city, options)
-  .then(response => response.json())
-  .then((response) => {
-
-    console.log(response)
-
-    cloud_pct.innerHTML = response.cloud_pct
-    temp.innerHTML = response.temp
-    temp2.innerHTML = response.temp
-    feels_like.innerHTML = response.feels_like
-    humidity.innerHTML = response.humidity
-    humidity2.innerHTML = response.humidity
-    min_temp.innerHTML = response.min_temp
-    max_temp.innerHTML = response.max_temp
-    wind_speed.innerHTML = response.wind_speed
-    wind_speed2.innerHTML = response.wind_speed
-    wind_degrees.innerHTML = response.wind_degrees
-    sunrise.innerHTML = response.sunrise
-    sunset.innerHTML = response.sunset
-  })
-  .catch(err => console.error(err));
+  fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city, options)
+    .then(response => response.json())
+    .then(response => {
+      document.getElementById('cloud_pct').innerHTML = response.cloud_pct;
+      document.getElementById('temp').innerHTML = response.temp;
+      document.getElementById('temp2').innerHTML = response.temp;
+      document.getElementById('feels_like').innerHTML = response.feels_like;
+      document.getElementById('humidity').innerHTML = response.humidity;
+      document.getElementById('humidity2').innerHTML = response.humidity;
+      document.getElementById('min_temp').innerHTML = response.min_temp;
+      document.getElementById('max_temp').innerHTML = response.max_temp;
+      document.getElementById('wind_speed').innerHTML = response.wind_speed;
+      document.getElementById('wind_speed2').innerHTML = response.wind_speed;
+      document.getElementById('wind_degrees').innerHTML = response.wind_degrees;
+      document.getElementById('sunrise').innerHTML = response.sunrise;
+      document.getElementById('sunset').innerHTML = response.sunset;
+    })
+    .catch(err => console.error(err));
 }
 
-submit.addEventListener("click", (e)=>{
-  e.preventDefault()
-  getWeather(city.value)
-})
+document.getElementById('submit').addEventListener('click', (e) => {
+  e.preventDefault();
+  getWeather(document.getElementById('city').value);
 
-getWeather("Delhi")
+  // Close the menu by removing the 'show' class
+  document.getElementById('nav-links').classList.remove('show');
+});
+
+getWeather("Patna");
